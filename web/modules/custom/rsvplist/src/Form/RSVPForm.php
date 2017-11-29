15645eb265e848b58e9599b7fa95116b62ceed08
ural <sevomax@gmail.com>
2017-11-29 17:56:45 -0500

    added form validation

--- web/modules/custom/rsvplist/src/Form/RSVPForm.php
+++ web/modules/custom/rsvplist/src/Form/RSVPForm.php
@@ -60,6 +60,18 @@ class RSVPForm extends FormBase {
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
-    drupal_set_message(t('The form is working.'));
+    $user = \Drupal\user\Entity\User::load(\Drupal::currentUser()->id());
+    $conn = Database::getConnection();
+    //db_insert('rsvplist') <- deprecated use $conn = Database::getConnection(); instead
+      $conn->insert('rsvplist')
+      ->fields(array(
+        'mail' => $form_state->getValue('email'),
+        'nid' => $form_state->getValue('nid'),
+        'uid' => $user->id(),
+        'created' => time(),
+      ))
+      ->execute();
+    drupal_set_message(t('Thank you for your RSVP, you are on the list for the event.'));
   }
+
 }
