f1d5cbda8184478950cd4e10a8ec5c85af1843a8
ural <sevomax@gmail.com>
2017-11-30 16:49:23 -0500

    added blockAccess to RSVPBlock.php

--- web/modules/custom/rsvplist/src/Plugin/Block/RSVPBlock.php
+++ web/modules/custom/rsvplist/src/Plugin/Block/RSVPBlock.php
@@ -22,8 +22,16 @@ class RSVPBlock extends BlockBase {
  * {@inheritdoc}
  */
   public function build() {
-    return array('#markup' => $this->t('My RSVP List Block'));
+    return \Drupal::formBuilder()->getForm('Drupal\rsvplist\Form\RSVPForm');
   }
-
+  public function blockAccess(AccountInterface $account) {
+    /** @var \Drupal\node\Entity\Node $node */
+    $node = \Drupal::routeMatch()->getParameter('node');
+    $nid = $node->nid->value;
+    if (is_numeric($nid)) {
+      return AccessResult::allowedIfHasPermission($account, 'view rsvplist');
+    }
+      return AccessResult::forbidden();
+  }
 
 }
