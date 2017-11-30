92364d9cb9b01f8898522cdb6f623def19ad219c
ural <sevomax@gmail.com>
2017-11-30 17:51:14 -0500

    added buildForm and submitForm to RSVPSettingsForm

--- web/modules/custom/rsvplist/src/Form/RSVPSettingsForm.php
+++ web/modules/custom/rsvplist/src/Form/RSVPSettingsForm.php
@@ -30,5 +30,33 @@ class RSVPSettingsForm extends ConfigFormBase {
     ];
   }
 
+  /**
+   * {@inheritcoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state, Request $request = NULL) {
+
+    $types = node_type_get_names();
+    $config = $this->config('rsvplist.settings');
+    $form['rsvplist_types'] = array(
+      '#type' => 'checkboxes',
+      '#title' => $this->t('The content types to enable RSVP collection for.'),
+      '#default_value' => $config->get('allowed_types'),
+      '#options' => $types,
+      '#description' => t('On the specified node types, an RSVP option will be available and can be enabled while tht node is being edited.'),
+    );
+    $form['array_filter'] = array('#type' => 'value', '#value' => TRUE);
+    return parent::buildForm($form, $form_state);
+  }
+  /**
+   * {@inheritcoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $allowed_types = array_filter($form_state->getValue('rsvplist_types'));
+    sort($allowed_types);
+    $this->config('rsvplist.settings')
+      ->set('allowed_types', $allowed_types)
+      ->save();
+    parent::submitForm($form, $form_state);
+  }
 
 }
