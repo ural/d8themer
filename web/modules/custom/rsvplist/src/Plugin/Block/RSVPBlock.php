9ec4640c8ea94ccb54e1ebfef715b2f79c7a33b7
ural <sevomax@gmail.com>
2017-11-30 16:29:18 -0500

    added Plugin/Block to rsvplist

---
+++ web/modules/custom/rsvplist/src/Plugin/Block/RSVPBlock.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @file
+ * Contains \Drupal\rsvplist\Plugin\Block\RSVPBlock
+ */
+
+namespace Drupal\rsvplist\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Session\AccountInterface;
+use Drupal\Core\Access\AccessResult;
+
+/**
+ * Provides RSVP List Block
+ * @Block(
+ *   id = "rsvp_block",
+ *   admin_label = @Translation("RSVP Block"),
+ * )
+ */
+class RSVPBlock extends BlockBase {
+/**
+ * {@inheritdoc}
+ */
+  public function build() {
+    return array('#markup' => $this->t('My RSVP List Block'));
+  }
+
+
+}
