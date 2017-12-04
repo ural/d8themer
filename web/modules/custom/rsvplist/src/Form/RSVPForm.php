c8dc97f9cb9c3b9314d6e707536e7274a0aea129
ural <sevomax@gmail.com>
2017-12-04 17:51:19 -0500

    added check for existing email to RSVPForm.php

--- web/modules/custom/rsvplist/src/Form/RSVPForm.php
+++ web/modules/custom/rsvplist/src/Form/RSVPForm.php
@@ -51,8 +51,19 @@ class RSVPForm extends FormBase {
   public function validateForm(array &$form, FormStateInterface $form_state) {
     $value = $form_state->getValue('email');
     if ($value == !\Drupal::service('email.validator')->isValid($value)) {
-      $form_state->setErrorByName('email', t('The email address %mail is not valid.', array('%mail' => $value)));
+      $form_state->setErrorByName('email', t('The email address %mail is not valid.', ['%mail' => $value]));
       return;
+    }
+    $node = \Drupal::routeMatch()->getParameter('node');
+    // Check if email already is set for this node
+    $select = Database::getConnection()->select('rsvplist', 'r');
+    $select->fields('r', array('nid'));
+    $select->condition('nid', $node->id());
+    $select->condition('mail', $value);
+    $results = $select->execute();
+    if (!empty($results->fetchCol())) {
+      //We found a row with this nid and email.
+      $form_state->setErrorByName('email', t('The address %mail is already subscribed to this list', array('%mail' => $value)));
     }
   }
 
