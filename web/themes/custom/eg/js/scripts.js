7bc5f0201065993facc700bc16d4db543694e5bb
ural <sevomax@gmail.com>
2017-12-15 21:12:33 -0500

    added scripts.js to libraries.yml

--- web/themes/custom/eg/js/scripts.js
+++ web/themes/custom/eg/js/scripts.js
@@ -10,13 +10,23 @@
   Drupal.behaviors.testScript = {
     attach: function (context, settings) {
 
-      $("#block-eg-main-menu > h2.removable-class", context).once('testScript', function () {
+      $(context).find('#block-eg-main-menu > h2.removable-class').once('testScript').each(function () {
         $(document).keyup(function (e) {
           if (e.keyCode == 27) {
             $("#block-eg-main-menu > h2.removable-class", context).removeClass("visually-hidden");
           }
         });
       });
+
+/*
+      $("#block-eg-main-menu > h2.removable-class", context).once('testScript').each(function () {
+        $(document).keyup(function (e) {
+          if (e.keyCode == 27) {
+            $("#block-eg-main-menu > h2.removable-class", context).removeClass("visually-hidden");
+          }
+        });
+      });
+*/
 
     }
   };
