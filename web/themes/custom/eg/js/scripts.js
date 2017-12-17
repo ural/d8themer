535f31f68ad82a9acc73fba3804c980505db5506
ural <sevomax@gmail.com>
2017-12-17 18:56:29 -0500

    playing with Drupal.behaviors

--- web/themes/custom/eg/js/scripts.js
+++ web/themes/custom/eg/js/scripts.js
@@ -18,17 +18,19 @@
         });
       });
 
-/*
-      $("#block-eg-main-menu > h2.removable-class", context).once('testScript').each(function () {
-        $(document).keyup(function (e) {
-          if (e.keyCode == 27) {
-            $("#block-eg-main-menu > h2.removable-class", context).removeClass("visually-hidden");
-          }
-        });
-      });
-*/
-
     }
   };
+
+  Drupal.behaviors.toggleShow = {
+    attach: function (context, settings) {
+
+      $(context).find(".view-display-id-block_1 h1").on("click", function () {
+
+        var toggleP = $(this).find("p:nth-child(2)");
+        toggleP.slideToggle("slow");
+      });
+
+    }
+  }
 
 })(jQuery);
