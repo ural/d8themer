f6cf21a28580293f97714fd49501e2baeea07a40
ural <sevomax@gmail.com>
2017-12-20 19:20:16 -0500

    added friendly-greeting.js

---
+++ web/themes/custom/eg/js/friendly-greeting.js
@@ -0,0 +1,17 @@
+(function (Drupal) {
+  // Override the default implementation of Drupal.theme.placeholder with our
+  // own custom one.
+  Drupal.theme.placeholder = function(str) {
+    return '<em class="friendly-placeholder">' + Drupal.checkPlain(str) + '</em>';
+  }
+
+  // If we have a nice user name, let's replace the
+  // site name with a greeting.
+  if (drupalSettings.friendly.name) {
+    //var siteName = document.getElementsByClassName('site-name')[0];
+    var siteLogo = $(".site-logo");
+    siteLogo.append('<h1>Howdy, ' + Drupal.theme('placeholder', drupalSettings.friendly.name) + '!</h1>');
+    //siteName.getElementsByTagName('a')[0].innerHTML = '<h1>Howdy, ' + Drupal.theme('placeholder', drupalSettings.friendly.name) + '!</h1>';
+  }
+
+})(Drupal);
