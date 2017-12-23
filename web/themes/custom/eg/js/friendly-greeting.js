e141b99cf6cd05fc00768ac84fcca772ad1be590
ural <sevomax@gmail.com>
2017-12-23 18:37:30 -0500

    playing with drupalSettings

--- web/themes/custom/eg/js/friendly-greeting.js
+++ web/themes/custom/eg/js/friendly-greeting.js
@@ -3,16 +3,15 @@
   // Override the default implementation of Drupal.theme.placeholder with our
   // own custom one.
   Drupal.theme.placeholder = function(str) {
-    return '<em class="friendly-placeholder">' + Drupal.checkPlain(str) + '</em>';
-  }
+    return '<span class="friendly-placeholder">' + Drupal.checkPlain(str) + '</span>';
+  };
 
   // If we have a nice user name, let's replace the
   // site name with a greeting.
+  var siteLogo = $("#block-eg-branding");
   if (drupalSettings.friendly.name) {
-    //var siteName = document.getElementsByClassName('site-name')[0];
-    var siteLogo = $("#block-eg-branding");
     siteLogo.append('<h4 class="howdy">Howdy, ' + Drupal.theme('placeholder', drupalSettings.friendly.name) + '!</h4>');
-    //siteName.getElementsByTagName('a')[0].innerHTML = '<h1>Howdy, ' + Drupal.theme('placeholder', drupalSettings.friendly.name) + '!</h1>';
   }
+  siteLogo.append('<span> @ TZ  ' + Drupal.theme('placeholder', drupalSettings.user_tz) + '</span>');
 
 })(jQuery, Drupal);
